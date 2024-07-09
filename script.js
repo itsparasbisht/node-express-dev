cf98e1c71158bc4c61048b7394942a42bd4f10c1
itsparasbisht <iamparasbisht@gmail.com>
2024-07-09 20:04:34 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+app.get("/", function (req, res) {
+  res.send("Hello World 👋");
+});
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}...`);
+});
